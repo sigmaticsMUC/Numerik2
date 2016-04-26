b8f7cac37bc42b259915c934a83296da052799f0
Alex <knoll1@hm.edu>
2016-04-26 02:58:12 +0200

    hochgeladen

--- Abgabe 1/Aufgabe 2/four.py
+++ Abgabe 1/Aufgabe 2/four.py
@@ -1,9 +1,23 @@
 import numpy as np
 from matplotlib import pyplot as mt
+# author: Knoll Alexander
 
 
 def explicit_euler(interval, h, y_0):
+    """
+    Computes numerical solution to u' = -2u + 1 using the explicit euler approach.
 
+    Input
+    -----
+    :param interval: list of equidistant values.
+    :param h: step size.
+    :param y_0: initial value at y(0).
+
+    Return
+    ------
+    :return ys: list containing values of solution.
+
+    """
     length = len(interval)
     ys = [None for _ in range(length)]
     ys[0] = y_0
@@ -14,7 +28,20 @@ def explicit_euler(interval, h, y_0):
 
 
 def implicit_euler(interval, h, y_0):
+    """
+    Computes numerical solution to u' = -2u + 1 using the implicit euler approach.
 
+    Input
+    -----
+    :param interval: list of equidistant values.
+    :param h: step size.
+    :param y_0: initial value at y(0).
+
+    Return
+    ------
+    :return ys: list containing values of solution.
+
+    """
     length = len(interval)
     ys = [None for _ in range(length)]
     ys[0] = y_0
@@ -25,7 +52,20 @@ def implicit_euler(interval, h, y_0):
 
 
 def adams_moulton2(interval, h, y_0):
+    """
+    Computes numerical solution to u' = -2u + 1 using the Adams-Moultan approach of second order.
 
+    Input
+    -----
+    :param interval: list of equidistant values.
+    :param h: step size.
+    :param y_0: initial value at y(0).
+
+    Return
+    ------
+    :return ys: list containing values of solution.
+
+    """
     length = len(interval)
     ys = [None for _ in range(length)]
     ys[0] = y_0
@@ -35,20 +75,36 @@ def adams_moulton2(interval, h, y_0):
 
 
 def main():
+    """
+    Setting necessary parameters for subroutines.
+    Also defines a lambda function representing the exact solution to u' = -2u + 1,
+    so that it can be compared to the numerical solutions.
+    Plotting results of exact solution along with results of the euler methods and the adams-moulton method.
 
-    l = 2.5
+    Return
+    ------
+    :return ys: list containing values of solution.
+
+    """
+    l = 10
     h = 0.1
+
+    # step size for exact solution
     h2 = 0.001
     epsilon = 0.0001
     y_0 = 1.0
     interval = np.arange(0, l+h, h)
+
+    # interval for exact solution
     interval2 = np.arange(0, l, h2)
 
+    # generating values for each approach
     explicit_euler_ys = explicit_euler(interval, h, y_0)
     exact_ys = list(map(lambda t: (0.5+epsilon)*np.e**(-2*t) + 0.5, interval2))
     implicit_euler_ys = implicit_euler(interval, h, y_0)
     moulton_ys = adams_moulton2(interval, h, y_0)
 
+    # plot results
     mt.figure()
     mt.xlabel('t')
     mt.ylabel('y(t)')
--- Abgabe 1/Aufgabe 2/two.py
+++ Abgabe 1/Aufgabe 2/two.py
@@ -1,4 +1,107 @@
 import numpy as np
 from matplotlib import pyplot as mt
+# author: Knoll Alexander
 
+
+def explicit_middle(interval, f, h, y_0):
+    """
+    Computes numerical solution to f using an explicit midpoint approach.
+
+    Input
+    -----
+    :param interval: list of equidistant values.
+    :param f: function describing the problem. f(t, y) = y'.
+    :param h: step size.
+    :param y_0: initial value at y(0).
+
+    Return
+    ------
+    :return ys: list containing values of solution.
+
+    """
+    length = len(interval)
+    ys = [None for _ in range(length)]
+
+    # set initial value
+    ys[0] = y_0
+    # provide further information so the numeric method can be executed
+    # using explicit euler to generate one more value.
+    ys[1] = y_0 + h * f(interval[0], y_0)
+
+    for i in range(1, length-1, 1):
+        ys[i+1] = ys[i-1] + 2*h*f(interval[i], ys[i])
+
+    return ys
+
+
+def implicit_bdf(interval, f, h, y_0):
+    """
+    Computes numerical solution to f using an implicit bdf approach.
+    The equation is solved by using a secant algorithm.
+
+    Input
+    -----
+    :param interval: list of equidistant values.
+    :param f: function describing the problem. f(t, y) = y'.
+    :param h: step size.
+    :param y_0: initial value at y(0).
+
+    Return
+    ------
+    :return ys: list containing values of solution.
+
+    """
+    length = len(interval)
+    ys = [None for _ in range(length)]
+
+    # set initial value
+    ys[0] = y_0
+    # provide further information so the numeric method can be executed
+    # using explicit euler to generate one more value.
+    ys[1] = y_0 + h * f(interval[0], y_0)
+
+    for i in range(1, length-1, 1):
+        U = (ys[i-1], ys[i])
+        func2 = lambda u: u - 4/3 * ys[i] + 1/3 * ys[i-1] - h * 2/3 * f(interval[i+1], u)
+        ys[i+1] = U[1] - (U[1]-U[0])/(func2(U[1]) - func2(U[0]))*func2(U[1])
+    return ys
+
+
+def main():
+    """
+    Defines a problem of the form y' = f(t, y), also an estimated solution at y(10) is necessary.
+    Afterwards using an implicite bdf an an explicit midpoint approach to compute approximations for different
+    step sizes h.
+
+    The resulting error values will be printed to the console.
+    """
+    l = 10
+    h = 0.1
+    y_0 = 1
+
+    #func = lambda t, u:  u*np.cos(t)**3 + u*np.sin(t) + 1/u+1
+    func = lambda t, u: -2 * u  + 1
+    exact_solution = 0.5000000010305768
+    h_list = (1, 10**-1, 10**-2, 10**-3, 10**-4, 10**-5)
+
+    length = len(h_list)
+    implicit_bdf_error = [None for _ in range(length)]
+    explicit_middle_error = [None for _ in range(length)]
+
+    for i in range(length):
+        interval = np.arange(0, l+h_list[i], h_list[i])
+        interval_length = len(interval)
+        implicit_bdf_error[i] = exact_solution - implicit_bdf(interval, func, h_list[i], y_0)[interval_length-1]
+        explicit_middle_error[i] = exact_solution - explicit_middle(interval, func, h_list[i], y_0)[interval_length-1]
+
+    print(implicit_bdf_error)
+    print(explicit_middle_error)
+
+    #interval = np.arange(0, l+h, h)
+    #res = explicit_middle(interval, func, h, y_0)
+    #mt.figure()
+    #mt.plot(interval, res, color='red')
+    #mt.show()
+
+main()
 
