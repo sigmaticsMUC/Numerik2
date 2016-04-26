a82599ebeeda0ba2c79742c21d1c209cc0824da6
Alex <knoll1@hm.edu>
2016-04-26 03:06:23 +0200

    init zweite abgabe

---
+++ Abgabe 2/Aufgabe 1/bar.py
@@ -0,0 +1,10 @@
+import numpy as np
+from matplotlib import pyplot as pt
+
+def finite_element():
+
+    return 0
+
+def difference():
+
+    return 0
---
+++ Abgabe 2/Aufgabe 2/chain.py
@@ -0,0 +1,8 @@
+import numpy as np
+from matplotlib import pyplot as pt
+
+
+def shoot():
+
+    return 0
+
